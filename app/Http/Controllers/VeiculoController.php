64e80101263c7f304a7f4b4ecd0f78704db3ce5d
Bruno Groth <bruno.groth@autoconf.com.br>
2022-02-04 13:04:27 -0300

    creation Model and Controller veiculo

---
+++ app/Http/Controllers/VeiculoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VeiculoController extends Controller
+{
+    public function create() {
+        dd('oi');
+    } 
+}
---
+++ database/migrations/2022_02_04_153407_create_veiculos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVeiculosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('veiculos', function (Blueprint $table) {
+            $table->id();
+            $table->char('placa', 7)->nullable($value = true);
+            $table->char('marca', 50);
+            $table->char('modelo', 50);
+            $table->decimal('valor_venda', $precision = 8, $scale = 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('veiculos');
+    }
+}
