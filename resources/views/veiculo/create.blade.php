e26ca585d50f9c923e564e6d377f81ddb3ccfa0c
bruno.groth <bruno.groth@autoconf.com.br>
2022-03-17 12:46:08 -0300

    created a template.blade with yield and stack for styles, contents and scripts

--- resources/views/veiculo/create.blade.php
+++ resources/views/veiculo/create.blade.php
@@ -1,14 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+@extends('layout.template', ['title' => 'Cadastro de Veículos'])
+
+@push('styles')
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Veículo - Bruno Veículos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<body>
-  @include('veiculo.menu')
+@endpush
+
+@section('content')
     <div class="container">
         <div class="row g-12 mt-3">
             <div class="col-auto">
@@ -20,9 +19,9 @@
             @include('veiculo.form')
         </form>
     </div>
+@endsection
     
-
- 
+ @push('scripts')
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.10/jquery.mask.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -32,5 +31,4 @@
             $('.plate').mask('SSS-0A00');
           });
     </script>
-</body>
-</html>
+@endpush
--- resources/views/veiculo/index.blade.php
+++ resources/views/veiculo/index.blade.php
@@ -1,14 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+@extends('layout.template', ['title' => 'Home - Bruno Veículos'])
+
+@push('styles')
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Veículos - Bruno Veículos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<body>
-  @include('veiculo.menu')
+@endpush
+
+@section('content')
     <div class="container">
         <div class="row justify-content-end">
             <div class="col-3">
@@ -74,7 +73,9 @@
         {{ $veiculos->appends(['search'=>$search])->links() }}
        </div>
     </div>
-
+@endsection
+    
+@push('scripts')
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.10/jquery.mask.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -84,11 +85,4 @@
             $('.plate').mask('SSS-0A00');
           });
     </script>
-    {{-- <script>
-      new Noty({
-        text: 'Some notification text',
-      }).show();
-  </script> --}}
-
-</body>
-</html>
+@endpush
