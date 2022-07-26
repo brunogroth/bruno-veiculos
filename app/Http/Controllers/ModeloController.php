b67cabdf8a3c80356a6acf739cd80ac5bd1b61b6
bruno.groth <bruno.groth@autoconf.com.br>
2022-07-26 15:49:44 -0300

    modelo exibir fk marca

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -12,14 +12,21 @@ class ModeloController extends Controller
 {
     public function index(Request $request){
         $modelos = Modelo::simplepaginate(5);
-
+        //$marca = Marca::find('id', $request->marca_id)->first();
+        //$marca = Marca::where('id',$request->marca_id)->get();
+        //$marca = Marca::select('descricao')->where('id', $request->marca_id)->get();
+        //$marca = Marca::where('id',5)->first();
         $search = $request->query('search');
+        
         if(!empty($search)){
-
+           
             $modelos = Modelo::where(
                 'descricao', 'like', '%'. $search .'%' 
             )->simplepaginate(5);
         }
+        $modelos->merge([
+            'marca' => $marca
+        ]);//ta dando pau aqui nao ta aparecendo o nome da marca la na view
         return view('modelos.index', compact('modelos', 'search')); 
     }
 
